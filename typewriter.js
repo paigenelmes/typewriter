e5126fce765f31d3a7ee9826832ea605a2433388
Paige Nelmes <designer@paigenelmes.com>
2022-10-15 14:26:09 -0700

    Adding Typewriter Exercise

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+const sentence = "hello there from lighthouse labs";
+//set initial speed to 1000
+let speed = 1000;
+//for loop over each character
+for (const char of sentence) {
+  //set the timeout and write each character
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, speed);
+  //increment speed by 50 for each character
+  speed += 50;
+}
+//adding the new line
+setTimeout(() => {
+  process.stdout.write('\n');
+}, speed);
